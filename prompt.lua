6aa7ce4f8e296b92136baabd92925f71df9cbb47
Corey Williamson <nerd1001001@gmail.com>
2020-02-21 06:08:33 -0600

    Improved prompt

--- prompt.lua
+++ prompt.lua
@@ -5,22 +5,31 @@ prompt.__index = prompt
 function prompt.new(question, default, callback, ...)
 	return setmetatable({
 		question = question,
-		default = default,
+		default = default or "",
 		callback = callback or function(input) 
 			return input
 		end,
 		callbackArgs = {},
 	}, prompt)
 end
-function prompt:__call()
+
+function prompt:draw()
 	io.write(self.question)
-	if self.default then
+	if self.default ~= "" then
 		io.write(" (", self.default, ")")
 	end
 	io.write(": ")
+end
+
+function prompt:__call()
+	self:draw()
 	local input = io.read()
 	if input == "" then input = self.default end
-	return self.callback(input, table.unpack(self.callbackArgs))
+	io.write(string.char(27) .. "[A")
+	io.write(string.char(27) .. "[K")
+	io.write(self.question, ": ", input, "\n")
+	self.callback(input, table.unpack(self.callbackArgs))
+	return input
 end
 
 return prompt
